3e54f0dad9ce5bbf6dd8e93545eb298dd34f5df2
Brena Lemos <brenavitorialemos@gmail.com>
2026-05-10 11:47:16 -0300

    Update foguete.gd

--- Cenas/foguete.gd
+++ Cenas/foguete.gd
@@ -1,29 +1,24 @@
 extends CharacterBody2D
 
 const VELOCIDADE_FRENTE = 200.0
-const VELOCIDADE_VERTICAL = 300.0
-
-const LIMITE_CIMA = 30
-const LIMITE_BAIXO = 310
+const VELOCIDADE_VERTICAL = 350.0
 
 func _physics_process(_delta):
 
-	var direcao_y = 0.0
+	var direcao_y = 0
 
-	# CONTROLE TECLADO
-	direcao_y = Input.get_axis("ui_up", "ui_down")
+	# PC
+	if Input.is_action_pressed("ui_up"):
+		direcao_y = -1
 
-	# CONTROLE TOUCH MOBILE
+	elif Input.is_action_pressed("ui_down"):
+		direcao_y = 1
+
+	# TOUCH MOBILE
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 
-		var toque_y = get_viewport().get_mouse_position().y
-		var metade_tela = get_viewport().get_visible_rect().size.y / 2
-
-		# TOCOU EM CIMA
-		if toque_y < metade_tela:
+		if get_viewport().get_mouse_position().y < get_viewport_rect().size.y / 2:
 			direcao_y = -1
-
-		# TOCOU EMBAIXO
 		else:
 			direcao_y = 1
 
@@ -32,8 +27,9 @@ func _physics_process(_delta):
 
 	move_and_slide()
 
+	# LIMITES AUTOMÁTICOS RESPONSIVOS
 	global_position.y = clamp(
 		global_position.y,
-		LIMITE_CIMA,
-		LIMITE_BAIXO
+		40,
+		get_viewport_rect().size.y - 40
 	)
