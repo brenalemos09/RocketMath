daf478b29852a0b836bb12edc2725fe89517458f
Brena Lemos <brenavitorialemos@gmail.com>
2026-05-10 11:46:32 -0300

    Update facil.gd

--- Cenas/facil.gd
+++ Cenas/facil.gd
@@ -5,30 +5,40 @@ extends Node
 var rng = RandomNumberGenerator.new()
 var timer_spawn: Timer
 
-const INTERVALO_SPAWN = 5
-const DISTANCIA_SPAWN_X = 700
-const MARGEM_CIMA = 80
-const MARGEM_BAIXO = 80
+const INTERVALO_SPAWN = 2.8
+const DISTANCIA_SPAWN_X = 900
+const MARGEM_CIMA = 60
+const MARGEM_BAIXO = 60
+const VELOCIDADE_ASTEROIDE = 180
 
 func _ready():
+
 	Progresso.iniciar_novo_jogo("facil")
-	#$MusicaAbertura.play()
 
 	rng.randomize()
 
 	criar_timer_spawn()
 
+	# POUCOS ASTEROIDES INICIAIS
 	for i in range(3):
 		spawnar_asteroide(300 + i * 250)
 
+
 func criar_timer_spawn():
+
 	timer_spawn = Timer.new()
+
 	timer_spawn.wait_time = INTERVALO_SPAWN
+
 	timer_spawn.autostart = true
+
 	timer_spawn.timeout.connect(spawnar_asteroide)
+
 	add_child(timer_spawn)
 
+
 func spawnar_asteroide(offset_x: float = DISTANCIA_SPAWN_X):
+
 	if cena_asteroide == null:
 		print("ERRO: cena_asteroide não foi colocada no Inspector")
 		return
@@ -40,12 +50,20 @@ func spawnar_asteroide(offset_x: float = DISTANCIA_SPAWN_X):
 		return
 
 	var asteroide = cena_asteroide.instantiate()
+
 	add_child(asteroide)
 
 	var altura_tela = get_viewport().get_visible_rect().size.y
-	var y_aleatorio = rng.randf_range(MARGEM_CIMA, altura_tela - MARGEM_BAIXO)
+
+	var y_aleatorio = rng.randf_range(
+		MARGEM_CIMA,
+		altura_tela - MARGEM_BAIXO
+	)
 
 	asteroide.global_position = Vector2(
 		foguete.global_position.x + offset_x,
 		y_aleatorio
 	)
+
+	# VELOCIDADE MAIS LENTA
+	asteroide.velocidade = VELOCIDADE_ASTEROIDE
